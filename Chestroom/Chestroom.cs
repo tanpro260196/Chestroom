e23f6e46c960093f6b40dc94143f992e54a8faae
LaurensDeV <devoogdlaurens@hotmail.com>
2016-06-25 15:37:05 +0200

    Improved plugin

--- Chestroom/Chestroom.cs
+++ Chestroom/Chestroom.cs
@@ -1,131 +1,121 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Terraria;
 using TShockAPI;
 
 namespace ChestroomPlugin
 {
-    public class Chestroom
-    {
-        static Random rnd = new Random();
+	public class Chestroom
+	{
 		public static int[] ExcludedItems = new int[] { 0, 2772, 2773, 2774, 2775, 2777, 2778, 2779, 2780, 2782, 2783, 2784, 2785, 2881, 3273, 3340, 3341, 3342, 3343, 3344, 3345, 3346, 3462, 3463, 3464, 3465, 3480, 3481, 3482, 3483, 3484, 3485, 3486, 3487, 3488, 3489, 3490, 3491, 3492, 3493, 3494, 3495, 3496, 3497, 3498, 3499, 3500, 3501, 3502, 3503, 3504, 3505, 3506, 3507, 3508, 3509, 3510, 3511, 3512, 3513, 3514, 3515, 3516, 3517, 3518, 3519, 3520, 3521, 3705, 3706 }; //67
-        static short[] torchframey = new short[] { 0, 22, 44, 66, 88, 110, 132, 154, 176, 198, 220, 242 };
-        static short[] platformframey = new short[] { 0, 18, 36, 54, 72, 90, 108, 144, 234, 228 };
-        static byte[] tiles = new byte[] { 38, 39, 41, 43, 44, 45, 47, 54, 118, 119, 121, 122, 140, 145, 146, 148, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 175, 176, 177, 189, 190, 191, 193, 194, 195, 196, 197, 198, 202, 206, 208, 225, 226, 229, 230, 248, 249, 250, 251, 252, 253 };
-        static byte[] walls = new byte[] { 4, 5, 6, 7, 9, 10, 11, 12, 19, 21, 22, 23, 24, 25, 26, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 41, 42, 43, 45, 46, 47, 72, 73, 74, 75, 76, 78, 82, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 109, 110, 113, 114, 115 };
-        static short[] chests = new short[] { 1, 3, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 };
+		static short[] torchframey = new short[] { 0, 22, 44, 66, 88, 110, 132, 154, 176, 198, 220, 242 };
+		static short[] platformframey = new short[] { 0, 18, 36, 54, 72, 90, 108, 144, 234, 228 };
+		static byte[] tiles = new byte[] { 38, 39, 41, 43, 44, 45, 47, 54, 118, 119, 121, 122, 140, 145, 146, 148, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 175, 176, 177, 189, 190, 191, 193, 194, 195, 196, 197, 198, 202, 206, 208, 225, 226, 229, 230, 248, 249, 250, 251, 252, 253 };
+		static byte[] walls = new byte[] { 4, 5, 6, 7, 9, 10, 11, 12, 19, 21, 22, 23, 24, 25, 26, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 41, 42, 43, 45, 46, 47, 72, 73, 74, 75, 76, 78, 82, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 109, 110, 113, 114, 115 };
+		static short[] chests = new short[] { 1, 3, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 };
 
-        public static int MaxItems =>  Main.maxItemTypes + 47;
-        public static int ActualMaxItems => MaxItems - ExcludedItems.Length; 
-        public static int MaxChests => (int)Math.Ceiling((decimal)ActualMaxItems / 40); 
-        public  static int ChestsPerRow => main.config.ChestsPerRow; 
-        public static int MaxRows => (int)Math.Ceiling((decimal)MaxChests / ChestsPerRow);
-        public  static int RowWidth => 4 * ChestsPerRow + 2; 
-        public  static int RowHeight => MaxRows * 5 + 1; 
+		public static int[] ItemIds { get; } = Enumerable.Range(-48, Main.maxItemTypes + 48).Where(i => !ExcludedItems.Contains(i)).ToArray();
+		public static int MaxChests => (int)Math.Ceiling((decimal)ItemIds.Length / 40);
+		public static int ChestsPerRow => main.config.ChestsPerRow;
+		public static int MaxRows => (int)Math.Ceiling((decimal)MaxChests / ChestsPerRow);
+		public static int RowWidth => 4 * ChestsPerRow + 2;
+		public static int RowHeight => MaxRows * 5 + 1;
 
-        public byte TileId;
-        public short ChestId;
-        public byte BackWall;
-        public short PlatformFrameY;
-        public short TorchFrameY;
+		public byte TileId;
+		public short ChestId;
+		public byte BackWall;
+		public short PlatformFrameY;
+		public short TorchFrameY;
 
-        public Chestroom(bool custom)
-        {
-            TileId = custom ? main.config.TileId : tiles[rnd.Next(0, tiles.Length)];
-            ChestId = custom ? main.config.ChestId : chests[rnd.Next(0, chests.Length)];
-            BackWall = custom ? main.config.BgWall : walls[rnd.Next(0, walls.Length)];
-            PlatformFrameY = custom ? main.config.pFrameY : platformframey[rnd.Next(0, platformframey.Length)];
-            TorchFrameY = custom ? main.config.tFrameY : torchframey[rnd.Next(0, torchframey.Length)];
-        }
-
-
-        public async Task<bool> Build(TSPlayer tsPlayer, int X, int Y)
-        {
-           return await Task.Run(() => {
-                int count = 0;
-                for (int ch = 0; ch < 1000; ch++)
-                {
-                    if (Main.chest[ch] != null)
-                        count++;
-                }
-                if (count > (1000 - MaxChests))
-                {
-                    tsPlayer.SendInfoMessage("Creating this chestroom would exceed the chest limit, chestroom cancelled.");
-                    return false;
-                }
-                bool first = true;
-                int placed = ChestsPerRow * MaxRows;
-                int chestitem = Main.maxItemTypes - 1;
+		public Chestroom(bool custom)
+		{
+			Random rnd = new Random();
+			TileId = custom ? main.config.TileId : tiles[rnd.Next(0, tiles.Length)];
+			ChestId = custom ? main.config.ChestId : chests[rnd.Next(0, chests.Length)];
+			BackWall = custom ? main.config.BgWall : walls[rnd.Next(0, walls.Length)];
+			PlatformFrameY = custom ? main.config.pFrameY : platformframey[rnd.Next(0, platformframey.Length)];
+			TorchFrameY = custom ? main.config.tFrameY : torchframey[rnd.Next(0, torchframey.Length)];
+		}
 
 
-                for (int y = RowHeight - 1; y >= 0; y--)
-                {
-                    for (int x = RowWidth - 1; x >= 0; x--)
-                    {
-                        if (x > 0 && x < RowWidth - 1 && y > 0 && y < RowHeight - 1)
-                        {
-                            Main.tile[x + X, y + Y] = new Tile() { wall = BackWall };
-                        }
-                        if (x == 0 || x == RowWidth - 1 || y == 0 || y == RowHeight - 1)
-                        {
-                            Main.tile[x + X, y + Y] = new Tile() { type = TileId };
-                            Main.tile[x + X, y + Y].active(true);
-                        }
-                        if (y % 5 == 0 && y > 0 && y < RowHeight - 1)
-                        {
-                            Main.tile[x + X, y + Y].active(true);
-                            if ((x % 4 == 2 || x % 4 == 3 || x == 1 || x == RowWidth - 2))
-                            {
-                                Main.tile[x + X, y + Y].type = TileId;
-                            }
-                            else if ((x % 4 == 0 || x % 4 == 1) && x > 1 && x < RowWidth - 2)
-                            {
-                                Main.tile[x + X, y + Y].type = (byte)19;
-                                Main.tile[x + X, y + Y].frameY = PlatformFrameY;
-                            }
-                        }
-                        if (y % 5 == 1 && (x == 1 || x == RowWidth - 2))
-                        {
-                            Main.tile[x + X, y + Y].active(true);
-                            Main.tile[x + X, y + Y].type = 4;
-                            Main.tile[x + X, y + Y].frameY = TorchFrameY;
-                        }
-                        if (y % 5 == 2 && x % 4 == 3 && x > 0 && x < RowWidth - 2)
-                        {
-                            placed--;
-                            if (placed < MaxChests)
-                            {
-                                WorldGen.AddBuriedChest(x + X, y + Y, 1, false, ChestId);
-                                if (Main.chest[count] != null)
-                                {
-                                    for (int i = 39; i >= 0; i--)
-                                    {
-                                        while (Utils.ExcludeItem(chestitem))
-                                            chestitem--;
+		public async Task<bool> Build(TSPlayer tsPlayer, int X, int Y)
+		{
+			return await Task.Run(() =>
+			{
+				int count = Main.chest.Count(c => c != null);
 
-                                        if (chestitem < -48)
-                                            break;
+				if (count > 1000 - MaxChests)
+				{
+					tsPlayer.SendInfoMessage("Creating this chestroom would exceed the chest limit, chestroom cancelled.");
+					return false;
+				}
+				bool first = true;
+				int placed = ChestsPerRow * MaxRows;
+				int itemIndex = ItemIds.Length - 1;
 
-                                        if (first)
-                                        {
-                                            i -= 40 - (ActualMaxItems + 1) % 40;
-                                            first = false;
-                                        }
-                                        Item itm = TShock.Utils.GetItemById(chestitem);
-                                        itm.stack = itm.maxStack;
-                                        Main.chest[count].item[i] = itm;
-                                        chestitem--;
-                                    }
-                                }
-                                count++;
-                            }
-                        }
-                    }
-                }
-                if (main.usinginfchests)
-                    Utils.ConvertToAutoRefill(count - MaxChests, MaxChests);
-                return true;
-            });
-        }           
-    }
+				for (int y = RowHeight - 1; y >= 0; y--)
+				{
+					for (int x = RowWidth - 1; x >= 0; x--)
+					{
+						if (x > 0 && x < RowWidth - 1 && y > 0 && y < RowHeight - 1)
+						{
+							Main.tile[x + X, y + Y] = new Tile() { wall = BackWall };
+						}
+						if (x == 0 || x == RowWidth - 1 || y == 0 || y == RowHeight - 1)
+						{
+							Main.tile[x + X, y + Y] = new Tile() { type = TileId };
+							Main.tile[x + X, y + Y].active(true);
+						}
+						if (y % 5 == 0 && y > 0 && y < RowHeight - 1)
+						{
+							Main.tile[x + X, y + Y].active(true);
+							if ((x % 4 == 2 || x % 4 == 3 || x == 1 || x == RowWidth - 2))
+							{
+								Main.tile[x + X, y + Y].type = TileId;
+							}
+							else if ((x % 4 == 0 || x % 4 == 1) && x > 1 && x < RowWidth - 2)
+							{
+								Main.tile[x + X, y + Y].type = (byte)19;
+								Main.tile[x + X, y + Y].frameY = PlatformFrameY;
+							}
+						}
+						if (y % 5 == 1 && (x == 1 || x == RowWidth - 2))
+						{
+							Main.tile[x + X, y + Y].active(true);
+							Main.tile[x + X, y + Y].type = 4;
+							Main.tile[x + X, y + Y].frameY = TorchFrameY;
+						}
+						if (y % 5 == 2 && x % 4 == 3 && x > 0 && x < RowWidth - 2)
+						{
+							if (--placed < MaxChests)
+							{
+								WorldGen.AddBuriedChest(x + X, y + Y, 1, false, ChestId);
+								if (Main.chest[count] != null)
+								{
+									for (int i = 39; i >= 0; i--)
+									{
+										if (itemIndex < 0)
+											break;
+										if (first)
+										{
+											i -= 40 - (ItemIds.Length) % 40;
+											first = false;
+										}
+										Item itm = TShock.Utils.GetItemById(ItemIds[itemIndex--]);
+										itm.stack = itm.maxStack;
+										Main.chest[count].item[i] = itm;
+									}
+								}
+								count++;
+							}
+						}
+					}
+				}
+				if (main.usinginfchests)
+					Utils.ConvertToAutoRefill(count - MaxChests, MaxChests);
+				return true;
+			});
+		}
+	}
 }
--- Chestroom/main.cs
+++ Chestroom/main.cs
@@ -13,195 +13,202 @@ using TShockAPI;
 
 namespace ChestroomPlugin
 {
-    [ApiVersion(1, 23)]
-    public class main : TerrariaPlugin
-    {
-        public static Config config = new Config();
-        public static IDbConnection Database;
-        public static bool usinginfchests;
+	[ApiVersion(1, 23)]
+	public class main : TerrariaPlugin
+	{
+		public static Config config = new Config();
+		public static IDbConnection Database;
+		public static bool usinginfchests;
 
-        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version; 
-        public override string Author => "Ancientgods";       
-        public override string Name => "Chestroom";       
-        public override string Description => "Generates a chestroom containing all items"; 
-        
-
-        public override void Initialize()
-        {
-            Commands.ChatCommands.Add(new Command("chestroom.create", chestroom, "chestroom", "cr") { AllowServer = false });
-            Commands.ChatCommands.Add(new Command("chestroom.reload", Reload_Config, "crreload"));
-
-            if (File.Exists(Path.Combine(Environment.CurrentDirectory, "ServerPlugins\\InfiniteChests.dll")))
-            {
-                switch (TShock.Config.StorageType.ToLower())
-                {
-                    case "mysql":
-                        string[] host = TShock.Config.MySqlHost.Split(':');
-                        Database = new MySqlConnection()
-                        {
-                            ConnectionString = string.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
-                                    host[0],
-                                    host.Length == 1 ? "3306" : host[1],
-                                    TShock.Config.MySqlDbName,
-                                    TShock.Config.MySqlUsername,
-                                    TShock.Config.MySqlPassword)
-                        };
-                        break;
-                    case "sqlite":
-                        string sql = Path.Combine(TShock.SavePath, "chests.sqlite");
-                        Database = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
-                        break;
-                }
-                usinginfchests = true;
-            }         
-            ReadConfig();
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-            }
-            base.Dispose(disposing);
-        }
-
-        public main(Main game): base(game)
-        {
-            Order = 1;
-        }
-
-        private void chestroom(CommandArgs args)
-        {
-            Stopwatch sw = Stopwatch.StartNew();
-            int X = args.Player.TileX;
-            int Y = args.Player.TileY;
-            string cmd = args.Parameters.Count > 0 ? args.Parameters[0].ToLower() : "";
-            int offsetX = 0,
-                offsetY = 0;
-            bool error = false;
-            if (cmd.Length != 2)
-                error = true;
-            for (int i = 0; i < cmd.Length; i++)
-            {
-                switch (cmd[i])
-                {
-                    case 't':
-                        offsetX -= -2;
-                        break;
-                    case 'b':
-                        offsetY -= Chestroom.RowHeight - 4;
-                        break;
-                    case 'l':
-                        offsetX -= 2;
-                        break;
-                    case 'c':
-                        offsetX -= (Chestroom.RowWidth - 2) / 2;
-                        break;
-                    case 'r':
-                        offsetX -= Chestroom.RowWidth - 4;
-                        break;
-                    default:
-                        error = true;
-                        break;
-                }
-            }
-            if (error)
-            {
-                args.Player.SendErrorMessage("Invalid syntax! Proper syntax: /chestroom <tl/tr/bl/br/tc/bc>");
-                args.Player.SendErrorMessage("t = top, l = left, r = right, b = bottom, c = center");
-                args.Player.SendErrorMessage("This is where you will stand when the chestroom spawns.");
-                return;
-            }
-            Chestroom chestRoom = new Chestroom(config.CustomRoom);
-            args.Player.SendSuccessMessage("Creating Chestroom...");
+		public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+		public override string Author => "Ancientgods";
+		public override string Name => "Chestroom";
+		public override string Description => "Generates a chestroom containing all items";
 
 
-            Task.Run(async () => {
-                bool success = await chestRoom.Build(args.Player, X + offsetX, Y + offsetY);
+		public override void Initialize()
+		{
+			Commands.ChatCommands.Add(new Command("chestroom.create", chestroom, "chestroom", "cr") { AllowServer = false });
+			Commands.ChatCommands.Add(new Command("chestroom.reload", Reload_Config, "crreload"));
+			Commands.ChatCommands.Add(new Command("chestroom.dump", (a) => 
+			{
+				Console.WriteLine("Dumping item type descriptions...");
+				ItemType.DumpItemTypeDescription();
+				Console.WriteLine("Done");
+			}, "crdump"));
 
-                if(success)
-                {
-                    sw.Stop();
-                    Utils.informplayers();
-                    args.Player.SendInfoMessage(string.Format("Chestroom created in {0} seconds. ({1} items in {2} chests)", sw.Elapsed.TotalSeconds, Chestroom.ActualMaxItems, Chestroom.MaxChests));
-                }
-            });
-        }
+			if (File.Exists(Path.Combine(Environment.CurrentDirectory, "ServerPlugins\\InfiniteChests.dll")))
+			{
+				switch (TShock.Config.StorageType.ToLower())
+				{
+					case "mysql":
+						string[] host = TShock.Config.MySqlHost.Split(':');
+						Database = new MySqlConnection()
+						{
+							ConnectionString = string.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
+									host[0],
+									host.Length == 1 ? "3306" : host[1],
+									TShock.Config.MySqlDbName,
+									TShock.Config.MySqlUsername,
+									TShock.Config.MySqlPassword)
+						};
+						break;
+					case "sqlite":
+						string sql = Path.Combine(TShock.SavePath, "chests.sqlite");
+						Database = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
+						break;
+				}
+				usinginfchests = true;
+			}
+			ReadConfig();			
+		}
 
-        public class Config
-        {
-            public int ChestsPerRow = (int)Math.Ceiling(Math.Sqrt(Chestroom.MaxChests));
-            public bool CustomRoom;
-            public byte TileId = 38;
-            public short ChestId = 1;
-            public byte BgWall = 4;
-            public short pFrameY = 18;
-            public short tFrameY = 22;
-        }
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+			}
+			base.Dispose(disposing);
+		}
 
-        static bool ReadConfig()
-        {
-            string filepath = Path.Combine(TShock.SavePath, "Chestroom.json");
-            try
-            {
-                if (File.Exists(filepath))
-                {
-                    using (var stream = new FileStream(filepath, FileMode.Open, FileAccess.Read, FileShare.Read))
-                    {
-                        using (var sr = new StreamReader(stream))
-                        {
-                            var configString = sr.ReadToEnd();
-                            config = JsonConvert.DeserializeObject<Config>(configString);
-                            config.ChestsPerRow = config.ChestsPerRow < 2 ? 2 : Math.Min(Chestroom.MaxChests, config.ChestsPerRow);
-                        }
-                        stream.Close();
-                    }
-                    return true;
-                }
-                else
-                {
-                    TShock.Log.ConsoleError("Chestroom config not found. Creating new one...");
-                    CreateConfig();
-                    return false;
-                }
-            }
-            catch (Exception ex)
-            {
-                TShock.Log.ConsoleError(ex.Message);
-            }
-            return false;
-        }
+		public main(Main game) : base(game)
+		{
+			Order = 1;
+		}
 
-        static void CreateConfig()
-        {
-            string filepath = Path.Combine(TShock.SavePath, "Chestroom.json");
-            try
-            {
-                using (var stream = new FileStream(filepath, FileMode.Create, FileAccess.Write, FileShare.Write))
-                {
-                    using (var sr = new StreamWriter(stream))
-                    {
-                        config = new Config();
-                        var configString = JsonConvert.SerializeObject(config, Formatting.Indented);
-                        sr.Write(configString);
-                    }
-                    stream.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                TShock.Log.ConsoleError(ex.Message);
-                config = new Config();
-            }
-        }
+		private void chestroom(CommandArgs args)
+		{
+			Stopwatch sw = Stopwatch.StartNew();
+			int X = args.Player.TileX;
+			int Y = args.Player.TileY;
+			string cmd = args.Parameters.Count > 0 ? args.Parameters[0].ToLower() : "";
+			int offsetX = 0,
+				offsetY = 0;
+			bool error = false;
+			if (cmd.Length != 2)
+				error = true;
+			for (int i = 0; i < cmd.Length; i++)
+			{
+				switch (cmd[i])
+				{
+					case 't':
+						offsetX -= -2;
+						break;
+					case 'b':
+						offsetY -= Chestroom.RowHeight - 4;
+						break;
+					case 'l':
+						offsetX -= 2;
+						break;
+					case 'c':
+						offsetX -= (Chestroom.RowWidth - 2) / 2;
+						break;
+					case 'r':
+						offsetX -= Chestroom.RowWidth - 4;
+						break;
+					default:
+						error = true;
+						break;
+				}
+			}
+			if (error)
+			{
+				args.Player.SendErrorMessage("Invalid syntax! Proper syntax: /chestroom <tl/tr/bl/br/tc/bc>");
+				args.Player.SendErrorMessage("t = top, l = left, r = right, b = bottom, c = center");
+				args.Player.SendErrorMessage("This is where you will stand when the chestroom spawns.");
+				return;
+			}
+			Chestroom chestRoom = new Chestroom(config.CustomRoom);
+			args.Player.SendSuccessMessage("Creating Chestroom...");
 
-        void Reload_Config(CommandArgs args)
-        {
-            if (ReadConfig())
-                args.Player.SendMessage("Chestroom config reloaded sucessfully.", Color.Green);
 
-            else
-                args.Player.SendErrorMessage("Chestroom config reloaded unsucessfully. Check logs for details.");
-        }
-    }
+			Task.Run(async () =>
+			{
+				bool success = await chestRoom.Build(args.Player, X + offsetX, Y + offsetY);
+
+				if (success)
+				{
+					sw.Stop();
+					Utils.informplayers();
+					args.Player.SendInfoMessage(string.Format("Chestroom created in {0} seconds. ({1} items in {2} chests)", sw.Elapsed.TotalSeconds, Chestroom.ItemIds.Length, Chestroom.MaxChests));
+				}
+			});
+		}
+
+		public class Config
+		{
+			public int ChestsPerRow = (int)Math.Ceiling(Math.Sqrt(Chestroom.MaxChests));
+			public bool CustomRoom;
+			public byte TileId = 38;
+			public short ChestId = 1;
+			public byte BgWall = 4;
+			public short pFrameY = 18;
+			public short tFrameY = 22;
+		}
+
+		static bool ReadConfig()
+		{
+			string filepath = Path.Combine(TShock.SavePath, "Chestroom.json");
+			try
+			{
+				if (File.Exists(filepath))
+				{
+					using (var stream = new FileStream(filepath, FileMode.Open, FileAccess.Read, FileShare.Read))
+					{
+						using (var sr = new StreamReader(stream))
+						{
+							var configString = sr.ReadToEnd();
+							config = JsonConvert.DeserializeObject<Config>(configString);
+							config.ChestsPerRow = config.ChestsPerRow < 2 ? 2 : Math.Min(Chestroom.MaxChests, config.ChestsPerRow);
+						}
+						stream.Close();
+					}
+					return true;
+				}
+				else
+				{
+					TShock.Log.ConsoleError("Chestroom config not found. Creating new one...");
+					CreateConfig();
+					return false;
+				}
+			}
+			catch (Exception ex)
+			{
+				TShock.Log.ConsoleError(ex.Message);
+			}
+			return false;
+		}
+
+		static void CreateConfig()
+		{
+			string filepath = Path.Combine(TShock.SavePath, "Chestroom.json");
+			try
+			{
+				using (var stream = new FileStream(filepath, FileMode.Create, FileAccess.Write, FileShare.Write))
+				{
+					using (var sr = new StreamWriter(stream))
+					{
+						config = new Config();
+						var configString = JsonConvert.SerializeObject(config, Formatting.Indented);
+						sr.Write(configString);
+					}
+					stream.Close();
+				}
+			}
+			catch (Exception ex)
+			{
+				TShock.Log.ConsoleError(ex.Message);
+				config = new Config();
+			}
+		}
+
+		void Reload_Config(CommandArgs args)
+		{
+			if (ReadConfig())
+				args.Player.SendMessage("Chestroom config reloaded sucessfully.", Color.Green);
+
+			else
+				args.Player.SendErrorMessage("Chestroom config reloaded unsucessfully. Check logs for details.");
+		}
+	}
 }
